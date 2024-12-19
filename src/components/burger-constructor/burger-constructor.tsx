ac0836913a66d1cc9066984829b64b8330c66f6e
Maksim Sergienko <creativesun@yandex.ru>
2024-12-20 00:16:36 +0300

    feat(burger-constructor): add tabs

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,4 +1,6 @@
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './style.module.css';
+import { useState } from 'react';
 
 /**
  * Текущий состав бургера
@@ -11,5 +13,38 @@ export const BurgerConstructor = () => {
     // типографику,
     // систему отступов.
     // TODO: Отображение списка организуйте самостоятельно. Подумайте над реализацией и возможным ограничением высоты блока, в том числе и на разных разрешениях экранов. Скроллбар не распространяется на заблокированные позиции конструктора.
-    return <div className={styles.container}>BurgerConstructor</div>;
+
+    const [current, setCurrent] = useState('Булки');
+
+    return (
+        <div className={styles.container}>
+            {/* TODO выделить в компонент  */}
+            <div
+                style={{ display: 'flex' }}
+                className="mb-10"
+            >
+                <Tab
+                    value="Булки"
+                    active={current === 'Булки'}
+                    onClick={setCurrent}
+                >
+                    Булки
+                </Tab>
+                <Tab
+                    value="Соусы"
+                    active={current === 'Соусы'}
+                    onClick={setCurrent}
+                >
+                    Соусы
+                </Tab>
+                <Tab
+                    value="Начинки"
+                    active={current === 'Начинки'}
+                    onClick={setCurrent}
+                >
+                    Начинки
+                </Tab>
+            </div>
+        </div>
+    );
 };
