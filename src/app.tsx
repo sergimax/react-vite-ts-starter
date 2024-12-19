21f42c838467828dc00533a88b7b5f3832612c48
Maksim Sergienko <creativesun@yandex.ru>
2024-12-20 00:00:29 +0300

    feat(app): use AppContent

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,12 @@
 import './app.css';
+import { AppContent } from './components/app-content';
 import { AppHeader } from './components/app-header';
-import { BurgerConstructor } from './components/burger-constructor';
-import { BurgerIngredients } from './components/burger-ingredients';
 
 function App() {
     return (
         <>
             <AppHeader />
-            <main>
-                <BurgerConstructor />
-                <BurgerIngredients />
-            </main>
+            <AppContent />
         </>
     );
 }
