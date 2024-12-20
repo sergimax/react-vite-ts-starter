6e7775b9a87a39aa326f836f140c5a18e4716e5c
Maksim Sergienko <creativesun@yandex.ru>
2024-12-20 22:42:25 +0300

    feat(burger-ingredients-category): add padding

--- src/components/burger-ingredients-category/burger-ingredients-category.tsx
+++ src/components/burger-ingredients-category/burger-ingredients-category.tsx
@@ -6,11 +6,12 @@ export const BurgerIngredientsCategory = ({
     category,
 }: BurgerIngredientsCategoryProps) => {
     const categoryTitleClass: string = `text_type_main-medium ${styles.title}`;
+    const categoryContentClass: string = `pb-10 pl-4 pr-4 pt-6 ${styles.content}`;
 
     return (
         <>
             <h2 className={categoryTitleClass}>{title}</h2>
-            <div className={styles.content}>
+            <div className={categoryContentClass}>
                 {category.map((element, index) => {
                     return <>{element.name}</>
                 })}
