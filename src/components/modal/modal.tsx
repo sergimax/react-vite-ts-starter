b26e4f82bcf8ff9c596ef3bf24d03106bbb65b30
Maksim Sergienko <creativesun@yandex.ru>
2024-12-26 00:10:14 +0300

    feat(modal): add Modal component

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,22 @@
+import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ModalProps } from './types';
+import styles from './modal.module.css';
+import { createPortal } from 'react-dom';
+
+export const Modal = ({ title = '', children }: ModalProps) => {
+    const modalClasses: string = `${styles.container}`;
+    const modalHeadingClasses: string = `pt-10 pl-10 pr-10 ${styles['modal-heading']}`;
+    const titleClasses: string = `text_type_main-large  ${styles.title}`;
+    const contentClasses: string = `pb-15 ${styles.content}`;
+
+    return createPortal(
+        <div className={modalClasses}>
+            <div className={modalHeadingClasses}>
+                <h2 className={titleClasses}>{title}</h2>
+                <CloseIcon type="primary" />
+            </div>
+            <div className={contentClasses}>{children}</div>
+        </div>,
+        document.body
+    );
+};
---
+++ src/components/modal/types.ts
@@ -0,0 +1,4 @@
+export type ModalProps = {
+    title: string;
+    children: JSX.Element;
+};
