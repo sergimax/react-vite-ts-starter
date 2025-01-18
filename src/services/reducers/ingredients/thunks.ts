13f4ba799343bc933ee2cd4a9b483541fe6a3e04
Maksim Sergienko <creativesun@yandex.ru>
2025-01-19 01:14:33 +0300

    feat(reducers): update ingredients reducer with data handling

--- src/services/reducers/ingredients/thunks.ts
+++ src/services/reducers/ingredients/thunks.ts
@@ -2,9 +2,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { INGREDIENTS_STATE_NAME } from './constants';
 import { API_ENDPOINT, API_URL } from '../../../constants/constants';
 import { FetchIngredientsAsyncThunkConfig } from './types';
+import { GetIngredientsDTO } from '../../../components/app/types';
 
 export const fetchIngredients = createAsyncThunk<
-    Response,
+    GetIngredientsDTO,
     void,
     FetchIngredientsAsyncThunkConfig
 >(`${INGREDIENTS_STATE_NAME}/fetch`, async (_, { rejectWithValue }) => {
@@ -18,7 +19,24 @@ export const fetchIngredients = createAsyncThunk<
             }
         );
 
-        return ingredientsResponse;
+        if (!ingredientsResponse.ok) {
+            throw new Error(`Статус ответа: ${ingredientsResponse.status}`);
+        }
+
+        const ingredientsData: GetIngredientsDTO =
+            await ingredientsResponse.json();
+
+        // Проверка успешности выполнения запроса
+        if (!ingredientsData.success) {
+            throw new Error(`Неуспешный статус загрузки`);
+        }
+
+        // Проверка пришедшего массива на наличие данных
+        if (!ingredientsData.data?.length) {
+            throw new Error(`Пустой список ингредиентов`);
+        }
+
+        return ingredientsData;
     } catch (error) {
         console.error('Произошла ошибка: ', error);
 
