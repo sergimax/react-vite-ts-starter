9914fd98f439eaf2e0e172c4a250b6981e8bed61
Maksim Sergienko <creativesun@yandex.ru>
2025-01-19 11:59:13 +0300

    feat(burger-ingredients): add tab change on scroll

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -19,6 +19,7 @@ export const BurgerIngredients = ({
 }: BurgerIngredientsProps) => {
     const titleClasses = `text_type_main-large pt-10 pb-5 ${styles.title}`;
     const [currentTab, setCurrentTab] = useState(ingredientCategories[0].value);
+    const [isScrolling, setIsScrolling] = useState(false);
 
     const bunsList: Array<Ingredient> = useMemo(
         () =>
@@ -48,44 +49,76 @@ export const BurgerIngredients = ({
     const categoriesRef = useRef<HTMLDivElement>(null);
 
     /**
-     * Переключение категорий ингредиентов с скроллом до  выбранной
+     * Переключение категорий ингредиентов с скроллом до выбранной
+     * @param tabName Наименование выбранной вкладки
+     * @param isOnClick Производится переключение по клику
      */
-    useEffect(() => {
+    function changeCurrentTab(
+        tabName: string,
+        isOnClick: boolean = false
+    ): void {
+        setCurrentTab(tabName);
+
+        if (!isOnClick) {
+            return;
+        }
+
         const categoriesRefCurrent = categoriesRef.current;
         if (!categoriesRefCurrent) {
             return;
         }
 
-        switch (currentTab) {
+        setIsScrolling(true);
+
+        switch (tabName) {
             case IngredientCategoryTypeLocalName.BUN:
                 bunsRef.current &&
-                    categoriesRefCurrent.scrollTo({
+                    categoriesRefCurrent.scrollBy({
                         top:
                             bunsRef.current.getBoundingClientRect().y -
                             categoriesRefCurrent.getBoundingClientRect().y,
                         behavior: 'smooth',
                     });
-                return;
+                break;
             case IngredientCategoryTypeLocalName.SAUCE:
                 saucesRef.current &&
-                    categoriesRefCurrent.scrollTo({
+                    categoriesRefCurrent.scrollBy({
                         top:
                             saucesRef.current.getBoundingClientRect().y -
                             categoriesRefCurrent.getBoundingClientRect().y,
                         behavior: 'smooth',
                     });
-                return;
+                break;
             case IngredientCategoryTypeLocalName.MAIN:
                 mainsRef.current &&
-                    categoriesRefCurrent.scrollTo({
+                    categoriesRefCurrent.scrollBy({
                         top:
                             mainsRef.current.getBoundingClientRect().y -
                             categoriesRefCurrent.getBoundingClientRect().y,
                         behavior: 'smooth',
                     });
-                return;
+                break;
         }
-    }, [currentTab]);
+        setIsScrolling(false);
+    }
+
+    function onCategoriesScroll() {
+        if (!categoriesRef.current || !bunsRef.current) {
+            return;
+        }
+
+        const delta =
+            categoriesRef.current.getBoundingClientRect().y -
+            bunsRef.current.getBoundingClientRect().y;
+
+        if (delta >= 0 && delta < 195) {
+            changeCurrentTab(IngredientCategoryTypeLocalName.BUN);
+        } else if (delta >= 195 && delta < 812) {
+            changeCurrentTab(IngredientCategoryTypeLocalName.SAUCE);
+        } else {
+            changeCurrentTab(IngredientCategoryTypeLocalName.MAIN);
+        }
+    }
 
     return (
         <section className={styles['app-content-block']}>
@@ -98,7 +131,7 @@ export const BurgerIngredients = ({
                             key={index}
                             value={category.value}
                             active={currentTab === category.value}
-                            onClick={setCurrentTab}
+                            onClick={(value) => changeCurrentTab(value, true)}
                         >
                             {category.title}
                         </Tab>
@@ -108,6 +141,7 @@ export const BurgerIngredients = ({
             {/* Блок категорий продуктов */}
             <div
                 className={styles.categories}
+                onScroll={() => !isScrolling && onCategoriesScroll()}
                 ref={categoriesRef}
             >
                 <BurgerIngredientsCategory
