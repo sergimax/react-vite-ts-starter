95a0509c461b819d218ac5dd664633c939ec9975
Maksim Sergienko <creativesun@yandex.ru>
2025-02-22 15:13:37 +0300

    feat(*): add Pages reducer

---
+++ src/services/reducers/pages/types.ts
@@ -0,0 +1,5 @@
+import { ROUTE_PATH } from '../../../components/app/constants';
+
+export type PagesState = {
+    active: ROUTE_PATH;
+};
--- src/services/reducers/reducers.ts
+++ src/services/reducers/reducers.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { ingredientsReducer } from './ingredients';
+import { pagesReducer } from './pages/slice';
 
 export const modelReducers = combineReducers({
     ingredients: ingredientsReducer,
+    pages: pagesReducer,
 });
 
 export const rootReducer = {
