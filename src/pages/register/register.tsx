9b0e8830c22b39947314c41f22366c059864f6b8
Maksim Sergienko <creativesun@yandex.ru>
2025-02-22 16:39:35 +0300

    feat(register): update layout

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { AppHeader } from '../../components/app-header';
 import { useAppDispatch, useAppSelector } from '../../services/hooks';
 import {
@@ -7,10 +7,22 @@ import {
 } from '../../services/reducers/pages';
 import { ROUTE_PATH } from '../../components/app/constants';
 
+import styles from './styles.module.css';
+import {
+    Button,
+    EmailInput,
+    Input,
+    PasswordInput,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+
 export const Register = () => {
     const dispatch = useAppDispatch();
 
     const activePage = useAppSelector(activePageSelector);
+
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
     useEffect(() => {
         dispatch(setActivePage({ value: ROUTE_PATH.REGISTER }));
@@ -19,7 +31,52 @@ export const Register = () => {
     return (
         <>
             <AppHeader activePage={activePage} />
-            register
+            <div className={styles.container}>
+                <div className={styles['register-form']}>
+                    <div className="text_type_main-medium">Регистрация</div>
+                    <Input
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Имя"
+                        name="name"
+                        value={name}
+                    ></Input>
+                    <EmailInput
+                        onChange={(e) => setEmail(e.target.value)}
+                        name="email"
+                        isIcon={false}
+                        value={email}
+                    ></EmailInput>
+                    <PasswordInput
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        name="password"
+                        icon="ShowIcon"
+                    ></PasswordInput>
+                    <Button
+                        htmlType="button"
+                        type="primary"
+                        size="medium"
+                        onClick={() => console.log('REGISTER')}
+                    >
+                        Зарегистрироваться
+                    </Button>
+                </div>
+                <div className={styles['additional-actions']}>
+                    <div
+                        className={
+                            'text text_type_main-default text_color_inactive'
+                        }
+                    >
+                        Уже зарегистрированы?{' '}
+                        <a
+                            className="text_color_accent"
+                            href={`${ROUTE_PATH.LOGIN}`}
+                        >
+                            Войти
+                        </a>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
