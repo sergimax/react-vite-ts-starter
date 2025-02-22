9282c1b1cff6ab3c00b094838cff8c1523d3dcaf
Maksim Sergienko <creativesun@yandex.ru>
2025-02-22 13:34:35 +0300

    feat(routes): setup basic routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { OrderConstructor } from '../../pages';
+import { ROUTE_PATH } from './constants';
 
 function App() {
     return (
@@ -7,9 +8,22 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route
+                        path={ROUTE_PATH.NOT_FOUND}
+                        element={<>404</>}
+                    />
+                    <Route path="*" />
+                    <Route
                         path="/"
                         element={<OrderConstructor />}
                     />
+                    <Route path={ROUTE_PATH.LOGIN} />
+                    <Route path={ROUTE_PATH.REGISTER} />
+                    <Route path={ROUTE_PATH.FORGOT_PASSWORD} />
+                    <Route path={ROUTE_PATH.RESET_PASSWORD} />
+                    <Route path={ROUTE_PATH.PROFILE} />
+                    <Route path={ROUTE_PATH.INGREDIENTS} />
+                    <Route path={ROUTE_PATH.ORDER_LIST} />
+                    <Route path={ROUTE_PATH.HISTORY} />
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/components/app/constants.ts
@@ -0,0 +1,11 @@
+export enum ROUTE_PATH {
+    LOGIN = '/login',
+    REGISTER = '/register',
+    FORGOT_PASSWORD = '/forgot-password',
+    RESET_PASSWORD = '/reset-password',
+    PROFILE = '/profile',
+    INGREDIENTS = '/ingredients',
+    ORDER_LIST = '/order-list',
+    HISTORY = '/history',
+    NOT_FOUND = '*',
+}
