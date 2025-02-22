8f75033d807ef684ed62a1a4bf78e44f396043b7
Maksim Sergienko <creativesun@yandex.ru>
2025-02-22 23:20:19 +0300

    feat(app-header): update buttons

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -4,57 +4,79 @@ import {
     Logo,
     ProfileIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components';
-import { AppHeaderProps } from './types';
 import styles from './style.module.css';
 import { ROUTE_PATH } from '../app/constants';
+import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../services/hooks';
+import {
+    activePageSelector,
+    setActivePage,
+} from '../../services/reducers/pages';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { ButtonParams } from './types';
 
 /**
  * Шапка приложения
  */
-export const AppHeader = ({ activePage }: AppHeaderProps) => {
+export const AppHeader = () => {
+    const navigate = useNavigate();
+
+    const activePage = useAppSelector(activePageSelector);
+
+    const constructorButtonParams: ButtonParams = getButtonParams(
+        ROUTE_PATH.DEFAULT
+    );
+    const orderListButtonParams: ButtonParams = getButtonParams(
+        ROUTE_PATH.ORDER_LIST
+    );
+    const profileButtonParams: ButtonParams = getButtonParams(
+        ROUTE_PATH.PROFILE
+    );
+
+    function getButtonParams(path: ROUTE_PATH): ButtonParams {
+        return {
+            class: `text_type_main-default ${styles['menu-button']} ${
+                path === activePage
+                    ? 'text_color_active'
+                    : 'text_color_inactive'
+            }`,
+            type: activePage === path ? 'primary' : 'secondary',
+        };
+    }
+
+    function handleClick(target: ROUTE_PATH): void {
+        navigate(target);
+    }
+
     return (
         <header>
             <nav className={styles.navigation}>
                 <div className={styles['left-icons']}>
-                    <a className={styles['menu-button']}>
-                        <BurgerIcon
-                            type={
-                                activePage === ROUTE_PATH.DEFAULT
-                                    ? 'primary'
-                                    : 'secondary'
-                            }
-                        />
-                        <span className="ml-2 text_type_main-default">
-                            Конструктор
-                        </span>
-                    </a>
-                    <a className={styles['menu-button']}>
-                        <ListIcon
-                            type={
-                                activePage === ROUTE_PATH.ORDER_LIST
-                                    ? 'primary'
-                                    : 'secondary'
-                            }
-                        />
-                        <span className="ml-2 text_type_main-default">
-                            Лента заказов
-                        </span>
-                    </a>
+                    <div
+                        className={constructorButtonParams.class}
+                        onClick={() => handleClick(ROUTE_PATH.DEFAULT)}
+                    >
+                        <BurgerIcon type={constructorButtonParams.type} />
+                        <span>Конструктор</span>
+                    </div>
+                    <div
+                        className={orderListButtonParams.class}
+                        onClick={() => handleClick(ROUTE_PATH.ORDER_LIST)}
+                    >
+                        <ListIcon type={orderListButtonParams.type} />
+                        <span>Лента заказов</span>
+                    </div>
                 </div>
                 <Logo className={styles.logo} />
                 <div className={styles['right-icons']}>
-                    <a className={styles['menu-button']}>
-                        <ProfileIcon
-                            type={
-                                activePage === ROUTE_PATH.PROFILE
-                                    ? 'primary'
-                                    : 'secondary'
-                            }
-                        />
-                        <span className="ml-2 text_type_main-default">
-                            Личный кабинет
-                        </span>
-                    </a>
+                    <div
+                        className={profileButtonParams.class}
+                        onClick={() => handleClick(ROUTE_PATH.PROFILE)}
+                    >
+                        <ProfileIcon type={profileButtonParams.type} />
+                        <span>Личный кабинет</span>
+                    </div>
                 </div>
             </nav>
         </header>
--- src/components/app-header/types.ts
+++ src/components/app-header/types.ts
@@ -3,3 +3,8 @@ import { ROUTE_PATH } from '../app/constants';
 export type AppHeaderProps = {
     activePage: ROUTE_PATH;
 };
+
+export type ButtonParams = {
+    class: string;
+    type: 'primary' | 'secondary';
+};
