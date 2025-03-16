791ea6a7f786c23e9000b723f484804d0841bf78
Maksim Sergienko <ififorgetyou@gmail.com>
2025-03-16 21:59:00 +0300

    feat(feed-list-item): add remainder number for hidden ingredients

--- src/components/feed-list-item/feed-list-item.tsx
+++ src/components/feed-list-item/feed-list-item.tsx
@@ -1,10 +1,65 @@
+import { CSSProperties, ReactNode } from 'react';
 import { FeedListItemStatus } from '../feed-list/types';
 import { Price } from '../price';
 import { FeedListItemProps } from './types';
+import { useAppSelector } from '../../services/hooks';
+import { ingredientsListSelector } from '../../services/reducers/ingredients/selectors';
 import styles from './styles.module.css';
 
 export const FeedListItem = ({ item }: FeedListItemProps) => {
+    const ingredients = useAppSelector(ingredientsListSelector);
     const statusClass: string = getStatusClass(item.status);
+    const ingredientsIds: Array<string> = [
+        item.ingredients.bunId,
+        ...item.ingredients.ingredientsIds,
+    ];
+    const ingredientsPreviews: ReactNode[] =
+        getIngredientPreviews(ingredientsIds);
+
+    /**
+     * Сформировать изображения первых 6 ингредиентов.
+     *
+     * Ингредиенты 7 и далее - скрываются, вместо них отображается число скрытых ингредиентов.
+     * @param ids
+     * @returns
+     */
+    function getIngredientPreviews(ids: Array<string>): ReactNode[] {
+        const idsToShow = ids.slice(0, 6);
+
+        const previews = idsToShow.map(id => {
+            return ingredients.find(item => item._id === id);
+        });
+
+        function getImageStyle(imageIndex: number): CSSProperties | undefined {
+            return {
+                transform: `translate(-${16 * imageIndex}px)`,
+                zIndex: 10 - imageIndex,
+            };
+        }
+
+        const previewImages = previews.map((item, index) => (
+            <img
+                src={item?.image_mobile}
+                className={styles['item-image-container']}
+                key={index}
+                style={getImageStyle(index)}
+            />
+        ));
+
+        if (ids.length > 6) {
+            const remainderClass = `${styles['remainded-ingredients-number']} text_type_main-default`;
+            previewImages[5] = (
+                <div className={styles['ingredient-item-with-number']}>
+                    {previewImages[5]}
+                    <div className={remainderClass} style={getImageStyle(5)}>
+                        +{ids.length - 5}
+                    </div>
+                </div>
+            );
+        }
+
+        return previewImages;
+    }
 
     function getStatusClass(status?: FeedListItemStatus): string {
         if (status === FeedListItemStatus.COMPLETED) {
@@ -32,8 +87,7 @@ export const FeedListItem = ({ item }: FeedListItemProps) => {
             </div>
             <div className={styles['item-content-and-price']}>
                 <div className={styles['item-content']}>
-                    {item.ingredients.bunId}
-                    {item.ingredients.ingredientsIds}
+                    {ingredientsPreviews}
                 </div>
                 <Price value={item.price} />
             </div>
