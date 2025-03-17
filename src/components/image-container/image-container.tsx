0c5b2dcebcc05f420d1907eb5664aca20ffcabd4
Maksim Sergienko <ififorgetyou@gmail.com>
2025-03-18 00:16:29 +0300

    feat(image-container): add ImageContainer

---
+++ src/components/image-container/image-container.tsx
@@ -0,0 +1,20 @@
+import { CSSProperties } from 'react';
+import { ImageContainerProps } from './types';
+import styles from './styles.module.css';
+
+export const ImageContainer = ({ src, index }: ImageContainerProps) => {
+    function getImageStyle(imageIndex: number): CSSProperties | undefined {
+        return {
+            transform: `translate(-${16 * imageIndex}px)`,
+            zIndex: 10 - imageIndex,
+        };
+    }
+
+    return (
+        <img
+            src={src}
+            className={styles['item-image-container']}
+            style={getImageStyle(index)}
+        />
+    );
+};
---
+++ src/components/image-container/types.ts
@@ -0,0 +1,4 @@
+export type ImageContainerProps = {
+    src: string;
+    index: number;
+};
