4209966314cdc4980dfc7ba62554823ff9778a92
Maksim Sergienko <ififorgetyou@gmail.com>
2025-03-20 01:30:57 +0300

    chore(feed-list): delete unused types

--- src/components/feed-list/types.ts
+++ src/components/feed-list/types.ts
@@ -1,11 +1,5 @@
 import { DataForModal, OrderDataFromWS } from "../../types/types";
 
-export enum FeedListItemTextStatus {
-    CREATED = 'Создан',
-    PROCESSING = 'Готовится',
-    COMPLETED = 'Выполнен',
-}
-
 export type FeedListProps = {
     orders: Array<OrderDataFromWS>;
     onItemClick: (data: DataForModal) => void;
